82e1cf82be7ab6a949934fc303dd10154109bb5c
Daniil <temnay2@icloud.com>
2025-03-09 00:02:30 +0300

    feature/UNISHARE-48: remove cookie headers from axios

--- src/shared/const/cookie.ts
+++ src/shared/const/cookie.ts
@@ -1,3 +1,1 @@
-export const CURRENT_SPACE_ID_COOKIE_KEY = 'currentSpaceId';
-
-export const CURRENT_SPACE_ID_COOKIE_HEADER = 'current-space-id';
+export const CURRENT_SPACE_ID_COOKIE_KEY = 'currentSpaceID';
